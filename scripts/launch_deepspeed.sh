60d48e82992e1e318cc1bcab6d2fd256817c5bd2
wayrise <102897795+wayrise@users.noreply.github.com>
2026-04-12 18:17:13 +0800

    chore: retain sanitized OpenWAM changes

--- scripts/launch_deepspeed.sh
+++ scripts/launch_deepspeed.sh
@@ -8,12 +8,30 @@ MAIN_PROCESS_IP="${MASTER_ADDR:-127.0.0.1}"
 MAIN_PROCESS_PORT="${MASTER_PORT:-29500}"
 TOTAL_GPUS=$(( NPROC_PER_NODE * NUM_MACHINES ))
 
+ACCELERATE_CONFIG=$(mktemp /tmp/accelerate_ds_XXXXXX.yaml)
+trap "rm -f ${ACCELERATE_CONFIG}" EXIT
+
+cat > "${ACCELERATE_CONFIG}" <<EOF
+compute_environment: LOCAL_MACHINE
+distributed_type: DEEPSPEED
+num_machines: ${NUM_MACHINES}
+num_processes: ${TOTAL_GPUS}
+machine_rank: ${MACHINE_RANK}
+main_process_ip: ${MAIN_PROCESS_IP}
+main_process_port: ${MAIN_PROCESS_PORT}
+mixed_precision: bf16
+
+deepspeed_config:
+  zero_stage: 2
+  gradient_accumulation_steps: 4
+  gradient_clipping: 1.0
+  offload_optimizer_device: none
+  offload_param_device: none
+  zero3_init_flag: false
+  zero3_save_16bit_model: false
+EOF
+
 accelerate launch \
-    --config_file configs/accelerate/deepspeed_zero2.yaml \
-    --num_machines "${NUM_MACHINES}" \
-    --num_processes "${TOTAL_GPUS}" \
-    --machine_rank "${MACHINE_RANK}" \
-    --main_process_ip "${MAIN_PROCESS_IP}" \
-    --main_process_port "${MAIN_PROCESS_PORT}" \
+    --config_file "${ACCELERATE_CONFIG}" \
     scripts/train.py \
     "$@"
