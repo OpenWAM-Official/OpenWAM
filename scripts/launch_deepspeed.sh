a4a6cf4d9b1701c34e63770a539029f4095f588b
wayrise <102897795+wayrise@users.noreply.github.com>
2026-04-12 18:55:49 +0800

    update accelerate to torchrun

--- scripts/launch_deepspeed.sh
+++ scripts/launch_deepspeed.sh
@@ -6,34 +6,16 @@ NUM_MACHINES="${NNODES:-${WORLD_SIZE:-1}}"
 MACHINE_RANK="${NODE_RANK:-${RANK:-0}}"
 MAIN_PROCESS_IP="${MASTER_ADDR:-127.0.0.1}"
 MAIN_PROCESS_PORT="${MASTER_PORT:-29500}"
-TOTAL_GPUS=$(( NPROC_PER_NODE * NUM_MACHINES ))
 
-unset WORLD_SIZE RANK LOCAL_RANK
+export ACCELERATE_USE_DEEPSPEED=true
+export ACCELERATE_DEEPSPEED_CONFIG_FILE=configs/deepspeed/zero2.json
+export ACCELERATE_MIXED_PRECISION=bf16
 
-ACCELERATE_CONFIG=$(mktemp /tmp/accelerate_ds_XXXXXX.yaml)
-trap "rm -f ${ACCELERATE_CONFIG}" EXIT
-
-cat > "${ACCELERATE_CONFIG}" <<EOF
-compute_environment: LOCAL_MACHINE
-distributed_type: DEEPSPEED
-num_machines: ${NUM_MACHINES}
-num_processes: ${TOTAL_GPUS}
-machine_rank: ${MACHINE_RANK}
-main_process_ip: ${MAIN_PROCESS_IP}
-main_process_port: ${MAIN_PROCESS_PORT}
-mixed_precision: bf16
-
-deepspeed_config:
-  zero_stage: 2
-  gradient_accumulation_steps: 4
-  gradient_clipping: 1.0
-  offload_optimizer_device: none
-  offload_param_device: none
-  zero3_init_flag: false
-  zero3_save_16bit_model: false
-EOF
-
-accelerate launch \
-    --config_file "${ACCELERATE_CONFIG}" \
+torchrun \
+    --nnodes "${NUM_MACHINES}" \
+    --nproc_per_node "${NPROC_PER_NODE}" \
+    --node_rank "${MACHINE_RANK}" \
+    --master_addr "${MAIN_PROCESS_IP}" \
+    --master_port "${MAIN_PROCESS_PORT}" \
     scripts/train.py \
     "$@"
